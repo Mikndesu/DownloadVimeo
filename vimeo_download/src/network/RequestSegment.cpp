cd359b10dece2772d304c5dfbe80b33ea89b2709
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-06-26 01:38:09 +0900

    renamed SegmentVideo

--- vimeo_download/src/network/RequestSegment.cpp
+++ vimeo_download/src/network/RequestSegment.cpp
@@ -1,7 +1,7 @@
-#include "SegmentVideo.hpp"
+#include "RequestSegment.hpp"
 #include <fstream>
 
-SegmentVideo::SegmentVideo(const std::string& url) : super(url) {
+RequestSegment::RequestSegment(const std::string& url) : super(url) {
     super::setCurlWriteFunction([](char *ptr, std::size_t size, std::size_t nmemb, void *user_data) {
                          std::ofstream *out = static_cast<std::ofstream *>(user_data);
                          size_t nbytes = size * nmemb;
@@ -10,7 +10,7 @@ SegmentVideo::SegmentVideo(const std::string& url) : super(url) {
                      });
 }
 
-void SegmentVideo::download(const std::string &path) {
+void RequestSegment::download(const std::string &path) {
     std::ofstream output(path, std::ios::binary | std::ios::app);
     curl_easy_setopt(super::curl, CURLOPT_FILE, &output);
     super::perform();
--- vimeo_download/src/network/RequestSegment.hpp
+++ vimeo_download/src/network/RequestSegment.hpp
@@ -1,9 +1,9 @@
 #pragma once
 #include "CurlStuff.hpp"
 
-class SegmentVideo : public CurlStuff {
+class RequestSegment : public CurlStuff {
     public:
     typedef CurlStuff super;
-    SegmentVideo(const std::string& url);
+    RequestSegment(const std::string& url);
     void download(const std::string &path);
 };
